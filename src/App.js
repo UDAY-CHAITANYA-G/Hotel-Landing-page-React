57c6e165c58599f98ca2fe11cdfb6b8167b8f7c1
UDAY-CHAITANYA-G <udaychaitanyagurvindapalli@gmail.com>
2023-10-03 00:46:32 +0530

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Single from './pages/Single';
 
 function App() {
   return (
     <div className="App">
-        <Home />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/:id" element={<Single />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Single/index.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import {useParams} from "react-router-dom";
+import properties from "../../data/properties";
+import Card from "../../components/Card";
+import "./style.css";
+
+export default function Single(){
+    let {id} = useParams();
+    const [data, setData] = useState({});
+    useEffect(()=>{
+        const data = properties.filter((props)=>{return props.id === Number(id)});
+        setData(...data);
+    },[id]);
+    return (
+    <div className="single">
+       {data && <Card name={data.name} address={data.address} city={data.city} image={data.image_link} room={data.room} bed={data.bed} bath={data.bath} area={data.area} price={data.price} popular={data.popular}/>}
+       {!data && <p>Invalid</p> }
+    </div>
+    );
+}
